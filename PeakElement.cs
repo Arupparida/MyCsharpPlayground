c59438c45dce2cb99f8ad496e286c74a237666cf
Arup Parida <arupggn@gmail.com>
2023-06-01 19:16:38 +0530

    peakvalueproblem

---
+++ PeakElement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyCsharpPlayground
+{
+    class PeakElement
+    {
+
+        static  int peakElement(int[] arr, int n)
+        {
+            int peak = 0;
+            if (n == 1)
+                return 0;
+            for (int i = 1; i < n; i++)
+            {
+                //int[] arr1 = new int[] { 1, 2, 6, 7 };
+                if (arr[i] > arr[i - 1])
+                {
+                    peak = i;
+                }
+            }
+            return peak;
+            //Console.WriteLine(peak);
+
+
+        }
+        public static void Main()
+        {
+            int[] arr1 = new int[] { 1, 2, 6, 7, 9,8,10,5};
+            int n = arr1.Length;
+            Console.Write("Index of a peak point is " +  peakElement(arr1, n));
+            //PeakElement nobj = new PeakElement();
+            //nobj.peakElement(arr1, n);
+
+        }
+
+
+    }
+}
+
+
+    
+
