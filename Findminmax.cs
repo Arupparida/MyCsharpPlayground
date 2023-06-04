74da1950157af84e6af00f410bfcf19b4c502576
Arup Parida <arupggn@gmail.com>
2023-06-04 18:56:54 +0530

    Added reverse number,Arr and string

--- Findminmax.cs
+++ Findminmax.cs
@@ -8,5 +8,24 @@ namespace MyCsharpPlayground
 {
     class Findminmax
     {
-    }
+        // C# code for the approach
+
+		static void Main()
+		{
+			// Input array
+			int[] arr = { 1, 423, 6, 46, 34, 23, 13, 53, 4 };
+			int n = arr.Length;
+			// Sort the array
+			Array.Sort(arr);
+
+			// after sorting the value at 0th position will be
+			// minimum and nth position will be maximum
+			Console.WriteLine("min-" + arr[0] + " max-"
+							+ arr[n - 1]);
+		}
+	}
+
+
+
 }
+
---
+++ Reversestring.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsharpPlayground
+{
+
+    //You are given a string s. You need to reverse the string.
+    class Reversestring
+    {
+        public static void Main()
+        {
+            
+            //Reverse the string
+            //Let's first reverse a num
+            int num = 56;
+            int rem;
+          
+            rem = num % 10;
+            num = rem * 10 + num / 10;
+            Console.WriteLine("Reversed num  : " + num);
+
+            //Reverse a string 
+            //It has something to do with index; swap index sequentially;
+            string s = "Kite";
+            int startindex;  int endindex;
+            int n = s.Length;
+
+            char[] StringArr = s.ToCharArray();
+            Array.Reverse(StringArr);
+            String reversedStr = new string(StringArr);
+            Console.WriteLine("Original str : " + s);
+            Console.WriteLine("Reversed Str : " + reversedStr);
+
+            /*for(int i =0; i < n - 1; i++)
+            {
+                startindex = i; 
+                endindex = n -1;
+                s[startindex] = s[endindex];
+              
+
+            }*/
+
+
+
+        }
+        
+    }
+}
