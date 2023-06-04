faef935beb56c7a2b5789a2748b9195870aeaca9
Arup Parida <arupggn@gmail.com>
2023-06-05 00:50:36 +0530

    reversse string by recursion

--- Reversestring.cs
+++ Reversestring.cs
@@ -12,10 +12,10 @@ namespace MyCsharpPlayground
     {
         public static void Main()
         {
-            
+            /*
             //Reverse the string
             //Let's first reverse a num
-            int num = 56;
+            int num = 5678;
             int rem;
           
             rem = num % 10;
@@ -23,16 +23,49 @@ namespace MyCsharpPlayground
             Console.WriteLine("Reversed num  : " + num);
 
             //Reverse a string 
+
             //It has something to do with index; swap index sequentially;
             string s = "Kite";
             int startindex;  int endindex;
             int n = s.Length;
+
+            //First easy method using Array.Reverse Funtion.
 
             char[] StringArr = s.ToCharArray();
             Array.Reverse(StringArr);
             String reversedStr = new string(StringArr);
             Console.WriteLine("Original str : " + s);
             Console.WriteLine("Reversed Str : " + reversedStr);
+
+            //Also discovered ReverseString is also a type.Maynot relevant in this context,
+            */
+
+            //Second Method using recursion
+            int start = 0;
+            int end = 5;
+
+            int temp;
+
+            int[] SArr = {1,2,3,4,5,6};
+
+            while (start < end)
+            {
+                temp = SArr[start];
+                SArr[start] = SArr[end];
+                SArr[end] = temp;
+                start++;
+                end--;
+
+            }
+
+            for(int i = 0; i < 6; i++)
+            {
+                Console.Write(SArr[i]+ " ");
+                
+            }
+
+
+
 
             /*for(int i =0; i < n - 1; i++)
             {
