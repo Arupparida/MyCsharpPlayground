b63408e3f4701ad3197acbf661874becd7f8920a
Arup Parida <arupggn@gmail.com>
2023-06-11 15:41:21 +0530

    palindrome-added

---
+++ Palindromecls.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsharpPlayground
+{
+    internal class Palindromecls
+    {   
+
+        /*
+         * Check if a given string is a palindrome. 
+                 
+         * 
+         */
+
+        //Invastigate code - recursive method
+        /* 
+         
+         
+        bool solution(string inputString)
+        {
+            int len = inputString.Length;
+            for (int i = 0; i < inputString.Length / 2 - 1; i++)
+            {
+                if (inputString[i] == inputString[len - 1])
+                    Console.WriteLine(inputString[len - 1]);
+                {
+                    i++;
+                    len--;
+                }
+
+
+            }
+            return true;
+            Console.WriteLine("false");
+
+
+
+        }
+
+        */
+
+
+
+
+        public static void Main()
+        {
+            //A palindrome number is a number that is same after reverse.
+            //For example 121, 34543, 343, 131, 48984 are the palindrome numbers.
+            //Write a program to check if number is a palindrome.
+            int reverse = 0;
+            int reminder = 0;
+           
+            Console.Write("Pls enter a number : ");
+            int number =  int.Parse(Console.ReadLine());
+            int temp = number;
+            //new thing learned here , just comment out temp in above step and use number variable 
+            //to compare to the reverse number. The result is wrong.
+
+            //int number = 34543;
+            //swap the number and check if they are same 
+            while (number > 0)
+            {
+                reminder = number % 10;
+                reverse = reverse * 10 + reminder;
+                number = number / 10;
+                
+            }
+           
+            Console.WriteLine("reverse is " + reverse); 
+            if (reverse == temp)
+            {
+                Console.WriteLine("It is a Palindrome");
+
+            }
+            else
+            {
+                Console.WriteLine("nil");
+            }
+                
+            
+
+            Palindromecls nobj = new Palindromecls();
+            nobj.solution("abac");
+            Console.WriteLine(nobj.solution("abac"));
+            
+
+
+        }
+
+    }
+}
---
+++ PrimeNumCls.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCsharpPlayground
+{
+    /*
+
+    Prime number is a number that is greater than 1 and divided by 1 or itself.
+    In other words, prime numbers can't be divided by other numbers than itself or 1.
+    For example 2, 3, 5, 7, 11, 13, 17, 19, 23.... are the prime numbers.
+    In this C# program, we will take an input from the user and check whether 
+    the number is prime or not.
+
+    */
+
+
+    internal class PrimeNumCls
+    {
+        public static void  Main()
+        {
+            //check if the given number is a prime number or not 
+            int num = int.Parse(Console.ReadLine());
+            bool flag = false;
+            Console.WriteLine("Pls enter a number to check for prime number");
+            for (int i = 2; i <= num; i++)
+
+            {
+
+                if (num % i == 0)
+                {
+                    flag = true;
+                    Console.WriteLine("It's a non-Prime Number");
+                    break;
+                }
+            
+                    
+                
+
+                if (flag == false)
+                {
+                    Console.WriteLine("It's a Prime Number");
+               
+                }
+
+
+
+
+
+
+
+            }
+            /*
+             * More problems related to Prime number  - src gfg
+             * Find two distinct prime numbers with a given product
+             * Print all prime numbers less than or equal to N
+             * Recursive program for prime number
+             * Find two prime numbers with a given sum
+             * Find the highest occurring digit in prime numbers in a range
+             * Prime Factorization using Sieve O(log n) for multiple queries
+             * Program to print all prime factors of a given number
+             * Least prime factor of numbers till n
+             * Prime factors of LCM of array elements – GeeksforGeeks
+             * Program for Goldbach’s Conjecture
+             * Prime numbers and Fibonacci
+             * Composite Number
+             * 
+                            
+             */
+
+        }
+    }
+    
+}
